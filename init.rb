9c8c828729595ea5dd4aae194ab7e77af016da3d
KemalC <ddeveloperr@gmail.com>
2016-07-23 22:10:20 +0000

    Fix Guide class when loading project

--- init.rb
+++ init.rb
@@ -7,8 +7,10 @@
 APP_ROOT = File.dirname(__FILE__)
 
 # require "#{APP_ROOT}/lib/guide"
+# require File.join(APP_ROOT, 'lib', 'guide.rb')
+# require File.join(APP_ROOT, 'lib', 'guide')
 
-$:.unshift(File.join(APP_ROOT, 'lib'))
+$:.unshift( File.join(APP_ROOT, 'lib') )
 require 'guide'
 
 guide = Guide.new('restaurants.txt')
