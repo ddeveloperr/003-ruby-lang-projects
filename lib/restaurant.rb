ebd421300fbeef3a8f9f71e6c9ca60ce5224877c
KemalC <ddeveloperr@gmail.com>
2016-07-24 22:46:10 +0000

    Handling input simple way

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -4,10 +4,25 @@ class Restaurant
       @@filepath = File.join(APP_ROOT, path)
     end
     
-    def self.file_exist? 
+    def self.file_exist?
+      if @@filepath && File.exists?(@@filepath)
+        return true
+      else
+        return false
+      end
+    end
+    
+    def self.file_usable?
+      return false unless @@filepath
+      return false unless File.exists?(@@filepath)
+      return false unless File.readable?(@@filepath)
+      return false unless File.writable?(@@filepath)
+      return true
     end
     
     def self.create_file
+      File.open(@@filepath, 'w') unless file_exist?
+      return file_usable?
     end
     
     def self.saved_restaurants
