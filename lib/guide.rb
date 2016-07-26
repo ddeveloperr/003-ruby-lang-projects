52f69db379c2e0fc534e57746f4455e3a4917e52
KemalC <ddeveloperr@gmail.com>
2016-07-26 01:39:51 +0000

    Improve list look

--- lib/guide.rb
+++ lib/guide.rb
@@ -59,11 +59,9 @@ class Guide
   end
   
   def list
-    puts "\nListing restaurants\n\n".upcase
+    output_action_header("Listing Restaurants")
     restaurants = Restaurant.saved_restaurants
-    restaurants.each do |rest|
-      puts rest.name + " | " + rest.causine + " | " + rest.formated_price
-    end
+    output_restaurant_table(restaurants)
   end
   
   def add
@@ -84,5 +82,27 @@ class Guide
 	def conclusion
   	puts "\n<<< Goodbye and Bon Appetit! >>>\n\n\n"
 	end
+	
+	private
+	
+	def output_action_header(text)
+	  puts "\n#{text.upcase.center(60)}\n\n"
+	end
+	
+	def output_restaurant_table(restaurants=[])
+    print " " + "Name".ljust(30)
+    print " " + "Cuisine".ljust(20)
+    print " " + "Price".rjust(6) + "\n"
+    puts "-" * 60
+    
+      restaurants.each do |rest|
+      line =  " " << rest.name.titleize.ljust(30)
+      line << " " + rest.cuisine.titleize.ljust(20)
+      line << " " + rest.formatted_price.rjust(6)
+      puts line
+      end
+    puts "No listings found" if restaurants.empty?
+    puts "-" * 60
+  end
 	
 end
